0fad3caf422a9ef09ce44c8255c4d19641d88c2f
Umar Farooq <aamirgel17@gmail.com>
2025-12-08 22:06:30 +0500

    Implement character arrays and string handling example
    Added a program to demonstrate character arrays and string input/output.

---
+++ Strings/Character arrays & strings.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	char str[] = {'a','b','c','\0'};
+	char str2[] = "Umar";
+
+	char str[100];
+	cout<<"Enter a string: ";
+	cin>>str;
+	
+	cout<<str;
+int n;
+cout<<"enter  anumber: ";
+cin>>n;
+cout<<n;
+
+	char str[] = "Umar Farooq";
+	for(int i=0; i<50; i++){
+		cout<<str[i]<<" ";
+	}
+	
+	return 0;
+}
